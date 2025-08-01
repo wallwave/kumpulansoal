a01229c820af75a267b3fb1e9b15b7b3d4db1b1b
WallWave <kakbay286@gmail.com>
2025-08-02 01:58:10 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -7,5 +7,12 @@ const firebaseConfig = {
   messagingSenderId: "1097394711054",
   appId: "1:1097394711054:android:97bfb2ca2ac774018baa5f"
 };
+const firebaseConfig = {
+  databaseURL: "https://kumpulan-soal-lengkap-default-rtdb.firebaseio.com/"
+};
+
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
+
+console.log("✅ Firebase initialized with URL:", firebaseConfig.databaseURL);
+
