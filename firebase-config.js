dec0b3d4f77f9a597e91872cb7fd76da6dd07479
WallWave <kakbay286@gmail.com>
2025-08-02 02:31:52 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,4 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBmvcUL3zYLc0frQ0hIRuyq8dUWTZGy3Zo",
-  authDomain: "kumpulan-soal-lengkap.firebaseapp.com",
-  databaseURL: "https://kumpulan-soal-lengkap-default-rtdb.firebaseio.com",
-  projectId: "kumpulan-soal-lengkap",
-  storageBucket: "kumpulan-soal-lengkap.appspot.com",
-  messagingSenderId: "1097394711054",
-  appId: "1:1097394711054:android:97bfb2ca2ac774018baa5f"
-};
+
 const firebaseConfig = {
   databaseURL: "https://kumpulan-soal-lengkap-default-rtdb.firebaseio.com/"
 };
