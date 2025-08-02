3a8422cd3c533f9b911c6651f17feca274c3be9a
WallWave <kakbay286@gmail.com>
2025-08-02 09:08:07 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,11 +5,4 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
-<<<<<<< HEAD
 
-console.log("✅ Firebase initialized with URL:", firebaseConfig.databaseURL);
-=======
-
-console.log("✅ Firebase initialized with URL:", firebaseConfig.databaseURL);
-
->>>>>>> d401f58bcc05103da0e2073320d046f079a7b1f5
